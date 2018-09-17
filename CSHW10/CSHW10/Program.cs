340ce2e174f9b8bde6e881e77f18d59812404561
Maslianka <nazar2131@ukr.net>
2018-09-17 15:31:10 +0300

    fiirst commit

---
+++ CSHW10/CSHW10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHW10
+{
+    class Program
+    {
+        // delegate declaration
+        public delegate int Del(int a);
+        public static int Tabulation(int a)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
