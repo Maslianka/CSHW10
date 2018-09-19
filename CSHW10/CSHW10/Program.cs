750ab50f6186558921f7a6801a55b95f64ec0caf
orest77 <orest.shkhumov@gmail.com>
2018-09-19 16:53:20 +0300

    d

--- CSHW10/CSHW10/Program.cs
+++ CSHW10/CSHW10/Program.cs
@@ -9,13 +9,29 @@ namespace CSHW10
     class Program
     {
         // delegate declaration
-        public delegate int Del(int a);
-        public static int Tabulation(int a)
-        {
+        public delegate double Del(double del);
 
+        public static double Sin(double x)
+        {
+            return 2 * Math.Pow(x, 2) + 3 * x * Math.Cos(Math.Pow(x, 3));
+        }
+
+        public static void Tabulation(Del del, double a, double b, int n)
+        {            
+            for(int k =0; k<=n; k++)
+            {
+                double x = (a + k * (b - a) / n);
+                Console.WriteLine($"{x} {del(x)}");
+            }
+             
         }
         static void Main(string[] args)
         {
+            Del del = Sin;
+            Tabulation( del, 4, 5, 6);
+            Console.WriteLine();
+            Console.ReadLine();
+
         }
     }
 }
